921953aa45cc633f89b3cb132942eb04100c3d5c
Daniel SIn <tysin@connect.ust.hk>
2023-02-12 04:15:11 +0800

    moved webscraper back

--- search/views.py
+++ search/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseRedirect
 from .forms import formResponse
-from ..summary.webscraper import WebpageScraper
+from .webscraper import WebpageScraper
 # Create your views here.
 
 
