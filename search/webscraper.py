7ff058741b5b57c91cb2ebd88eb1afc8b63d1f4e
Daniel SIn <tysin@connect.ust.hk>
2023-02-12 05:09:53 +0800

    Update webscraper.py

--- search/webscraper.py
+++ search/webscraper.py
@@ -4,17 +4,21 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
+
 import chromedriver_autoinstaller
 
 
 class WebpageScraper():
     def __init__(self):
         # chromedriver_autoinstaller.install() 
-        chrome_options = Options()
-        chrome_options.add_argument("--headless")
-        chrome_options.add_argument('--no-sandbox')
-        chrome_options.add_argument('--disable-dev-shm-usage')
-        self.browser = webdriver.Chrome(ChromeDriverManager(path=r'/tmp').install(),chrome_options=chrome_options)
+        # chrome_options = Options()
+        # chrome_options.add_argument("--headless")
+        # chrome_options.add_argument('--no-sandbox')
+        # chrome_options.add_argument('--disable-dev-shm-usage')
+        self.browser = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+        # self.browser = webdriver.Chrome(ChromeDriverManager(path=r'/tmp').install(),chrome_options=chrome_options)
 
     def get(self,url):
         self.browser.get(url)
