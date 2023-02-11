c49a4c753cc030243823085eb3b3d3b3a6bf8f95
Daniel SIn <tysin@connect.ust.hk>
2023-02-11 21:47:33 +0800

    Initial commit

---
+++ search/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from django.http import HttpResponse,HttpResponseRedirect
+from .forms import formResponse
+# Create your views here.
+# from selenium import webdriver
+# from bs4 import BeautifulSoup
+# from webdriver_manager.chrome import ChromeDriverManager
+# from selenium.webdriver.chrome.options import Options
+
+# chrome_options = Options()
+# chrome_options.add_argument("--headless")
+
+# browser = webdriver.Chrome(ChromeDriverManager().install(),chrome_options=chrome_options)
+
+def search(request):
+    # return render(request,"home.html")
+     # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = formResponse(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            a=form.cleaned_data['your_name']
+            return render(request,'done.html',{'name':a})
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = formResponse()
+
+    return render(request, 'home.html', {'form': form})
