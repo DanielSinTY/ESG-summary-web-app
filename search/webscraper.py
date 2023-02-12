4df334d422c9f9be9f801a87292c6d7f5f263ef1
Daniel SIn <tysin@connect.ust.hk>
2023-02-12 12:23:31 +0800

    Update webscraper.py

--- search/webscraper.py
+++ search/webscraper.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 # from bs4 import BeautifulSoup
+
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service as ChromeService
@@ -8,6 +9,28 @@ from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
 
 import chromedriver_autoinstaller
+# script.py
+import os
+import sys
+import subprocess
+import platform
+from selenium import webdriver
+
+def install_chromedriver(version):
+    os_type = platform.system().lower()
+    chromedriver_path = f"chromedriver_{os_type}64"
+    if not os.path.exists(chromedriver_path):
+        chromedriver_url = f"https://chromedriver.storage.googleapis.com/{version}/chromedriver_{os_type}64.zip"
+        subprocess.run(["wget", chromedriver_url])
+        subprocess.run(["unzip", f"chromedriver_{os_type}64.zip"])
+        subprocess.run(["rm", f"chromedriver_{os_type}64.zip"])
+        os.environ["PATH"] += os.pathsep + os.getcwd()
+
+# specify the desired version of ChromeDriver
+version = "87.0.4280.20"
+install_chromedriver(version)
+
+
 
 
 class WebpageScraper():
@@ -21,12 +44,9 @@ class WebpageScraper():
         # self.browser = webdriver.Chrome(ChromeDriverManager(path=r'/tmp').install(),chrome_options=chrome_options)
         
 
-        # Set up the Chrome options to run in headless mode
-        options = Options()
-        options.headless = True
-
-        # Initialize the webdriver
-        driver = webdriver.Chrome(options=options)
+        options = webdriver.ChromeOptions()
+        options.add_argument("--headless")
+        self.browser = webdriver.Chrome(options=options)
 
     def get(self,url):
         self.browser.get(url)
